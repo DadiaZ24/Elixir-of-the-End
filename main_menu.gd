cf68595db9363483a2145739cbe738830d53afb6
maugusto <marcoamorim911@gmail.com>
2025-05-30 21:50:48 +0100

    start path finding

--- main_menu.gd
+++ main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
-func _ready():
-	$playButton.grab_click_focus()
+#func _ready():
+	#$playButton.grab_click_focus()
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
