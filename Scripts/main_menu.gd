390c09b8b061f0687fcbef1cbe088a7d6e7fce07
Dot99 <gui.rocha.felix@gmail.com>
2025-05-31 09:04:21 +0100

    Transition

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var transition = $Transition
+@onready var colorRect = $Transition/ColorRect
+#func _ready():
+	#$playButton.grab_click_focus()
+
+func _on_play_button_pressed():
+	colorRect.visible = true;
+	transition.play("fade_in")
+
+func _on_transition_animation_finished(anim_name):
+	colorRect.visible = false;
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit(0)
