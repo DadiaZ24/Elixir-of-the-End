23abea84b89c5e04e32ba42e7830b00249cb7bb7
Dot99 <gui.rocha.felix@gmail.com>
2025-05-31 20:12:28 +0100

    Change text

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -13,7 +13,7 @@ var tutorial_steps = [
 	{"text": "Move your [b]mouse[/b] to look around", "action": "mouse_move"},
 	{"text": "Press [b]Space[/b] to jump", "action": "ui_accept"},
 	{"text": "Press [b]SHIFT[/b] to sprint", "action": "ui_sprint"},
-	{"text": "[color=lime][b]Tutorial Complete! You're ready.[/b][/color]", "action": ""}
+	{"text": "[color=lime][b]Tutorial Complete! Go talk to your master.[/b][/color]", "action": ""}
 ]
 
 var current_step = 0
