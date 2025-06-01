6952e03519eed2c98bbc3d2f3d49544f4d7006ec
maugusto <marcoamorim911@gmail.com>
2025-06-01 18:32:37 +0100

    adding more items

--- Scripts/nenufar.gd
+++ Scripts/nenufar.gd
@@ -2,10 +2,12 @@ extends StaticBody3D
 
 @onready var area = $Area3D
 @onready var nenufar_mesh = $Onthewater
-@onready var nenufar_scene = "res://Scenes/assets/nenufar.tscn"
+@onready var nenufar_scene = preload("res://Scenes/assets/nenufar.tscn")
+@onready var nenufar_tres = preload("res://Scripts/inventory/resources/Ingredients/something_water.tres")
 @onready var normal_material = nenufar_mesh.get_surface_override_material(0)
 
 var player_near = false
+var player = null
 
 func _ready():
 	area.body_entered.connect(_on_body_entered)
@@ -14,10 +16,12 @@ func _ready():
 func _on_body_entered(body):
 	if body.name == "Player":
 		player_near = true
+		player = body
 
 func _on_body_exited(body):
 	if body.name == "Player":
 		player_near = false
+		player = null
 		nenufar_mesh.set_surface_override_material(0, normal_material)
 
 func _process(delta):
@@ -25,6 +29,31 @@ func _process(delta):
 		return
 
 	if Input.is_action_just_pressed("ui_interact"):
+		#INVENTORY ADD LOGIC
+		var inventory_data = player.inventory_data
+		if inventory_data:
+			var new_slot_data := SlotData.new()
+			new_slot_data.item_data = nenufar_tres
+			new_slot_data.quantity = 1
+
+			var inserted := false
+			for i in range(inventory_data.slot_datas.size()):
+				var existing = inventory_data.slot_datas[i]
+				if existing == null:
+					inventory_data.slot_datas[i] = new_slot_data
+					inserted = true
+					break
+				elif existing.can_merge_with(new_slot_data):
+					existing.fully_merge_with(new_slot_data)
+					inserted = true
+					break
+
+			if inserted:
+				inventory_data.inventory_updated.emit(inventory_data)
+				queue_free()
+			else:
+				print("No room in inventory!")
+		#OTHER LOGIC
 		var camera = get_node("/root/Main/Player/Neck/Camera3D")
 		var ray = camera.get_node("InteractRay")
 		queue_free()
