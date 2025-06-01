c72d6f19b885daf0ede83932c3149e0fac208b15
ddias-fe <ddias-fe@student.42porto.com>
2025-06-01 16:35:39 +0100

    fix: fixed some bugs

--- Scripts/master_tent.gd
+++ Scripts/master_tent.gd
@@ -1,4 +1,25 @@
 extends Node3D
 
+@onready var player: CharacterBody3D = $Player
+@onready var inventory_interface: Control = $InventoryUI/InventoryInterface
+
 func _ready() -> void:
 	GameState.next_spawn_point_name = "master"
+	player.toggle_inventory.connect(toggle_inventory_interface)
+	inventory_interface.set_player_inventory_data(player.inventory_data)
+	for node in get_tree().get_nodes_in_group("crafting_inventory"):
+		node.toggle_inventory.connect(toggle_inventory_interface)
+
+func toggle_inventory_interface(external_inventory_owner = null) -> void:
+	inventory_interface.visible = not inventory_interface.visible
+	
+	if inventory_interface.visible:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	if external_inventory_owner:
+		inventory_interface.set_external_inventory(external_inventory_owner)
+	else:
+		inventory_interface.clear_external_inventory()
+		
