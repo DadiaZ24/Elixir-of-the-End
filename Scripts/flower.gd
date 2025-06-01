bf90ff8a13429f3106c234e24912efdbe23c0383
ddias-fe <ddias-fe@student.42porto.com>
2025-06-01 14:03:08 +0100

    fix: fixed interaction

--- Scripts/flower.gd
+++ Scripts/flower.gd
@@ -24,7 +24,7 @@ func _process(delta):
 	if not player_near:
 		return
 
-	if Input.is_action_just_pressed("interact"):
+	if Input.is_action_just_pressed("ui_grab"):
 		var camera = get_node("/root/Main/Player/Neck/Camera3D")
 		var ray = camera.get_node("InteractRay")
 		queue_free()
